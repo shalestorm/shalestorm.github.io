02e0c247d924d796ddced1987f3611b08dba3213
shalestorm <45797982+shalestorm@users.noreply.github.com>
2023-04-19 13:15:47 -0400

    Add files via upload

---
+++ webapps/expand_collapse/subset_expansion.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $("a").click(function() { 
+    
+    $(this).prev().toggleClass("hide");    
+    if ($(this).prev().hasClass("hide")) {
+      $(this).text("Show more");
+    } else {
+      $(this).text("Show less");
+    }
+    
+  });
+
+});
